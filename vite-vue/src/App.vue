be05b6bae49f600bcd31724b4e43a5f543d5498c
Marcus Sanchez <marcussanchez031@gmail.com>
2023-06-30 22:35:38 -0400

    basic counter

--- vite-vue/src/App.vue
+++ vite-vue/src/App.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
 
-  const count = ref(0);
+import { ref } from 'vue';
+
+const count = ref(0);
 
 </script>
-  <div>
-    <button @click="count++" class="_">count is: {{ count }}</button>
 
-  </div>
 <template>
-
+  <div class="flex justify-center items-center min-h-screen flex-col">
+    <button @click="count++" class="border-2 border-black p-1 ">Increment</button>
+    <h3>count is: {{ count }}</h3>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
-
